6c29ef3b77edbb703f5bf414f7cd0586a8e5e9cd
wz@dell <wz@dell.com>
2023-12-26 21:23:19 +0800

    新增toml关系依赖版本

--- app/src/main/java/com/example/basemvicompose/MainActivity.kt
+++ app/src/main/java/com/example/basemvicompose/MainActivity.kt
@@ -4,19 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.basemvicompose.ui.theme.BaseMVIComposeTheme
+import com.example.basemvicompose.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BaseMVIComposeTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -31,6 +32,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Card {
+
+    }
     Text(
         text = "Hello $name!",
         modifier = modifier
@@ -40,7 +44,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    BaseMVIComposeTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
